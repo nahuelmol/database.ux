4e30e4c6ae7d397a09b36cf36c0337aceae39431
Silvio Molina <molinahuel44@gmail.com>
2021-08-10 20:56:34 -0300

    more files

--- interface/start.py
+++ interface/start.py
@@ -19,10 +19,29 @@ class Application(tk.Frame):
 		self.quit.pack(side="bottom")
 
 	def deploy_more_buttons(self):
-		print("\nDeploying new buttons and its functions")
+		self._add_country = tk.Button(self)
+		self._add_country["text"] = "Add country"
+		self._add_country["command"] = self.add_country
+		self._add_country.pack(side="right")
+
+		self._delete_country = tk.Button(self)
+		self._delete_country["text"] = "Delete country"
+		self._delete_country["command"] = self.delete_country
+		self._delete_country.pack(side="right")
+
+		self._update_country = tk.Button(self)
+		self._update_country["text"] = "Update country"
+		self._update_country["command"] = self.update_country
+		self._update_country.pack(side="right")
 
 	def add_country(self):
 		print("\nTime to add some country")
+
+	def delete_country(self):
+		print("\nTime to delete a country")
+
+	def update_country(self):
+		print("\nTime to update a country")
 
 
 if __name__ == "__main__":
@@ -38,4 +57,3 @@ if __name__ == "interface.start":
 	app = Application(master=root)
 	app.mainloop()
 
-print(__name__)
